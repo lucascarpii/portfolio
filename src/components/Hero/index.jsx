e206377be1009b9aaf7c0dc4eede61a33dfcce9b
Lucas Fuentes <lucascarpii2002@gmail.com>
2023-12-08 18:30:52 -0300

    <feat> Crear componente hero para datos basicos

---
+++ src/components/Hero/index.jsx
@@ -0,0 +1,14 @@
+
+const Hero = () => {
+  return(
+    <section className="grid grid-cols-[100px_auto] gap-4 sm:py-6">
+      <img src="/favicon.svg" alt="" className="bg-neutral-800 rounded-full" />
+      <div className="flex flex-col items-start justify-center">
+        <h2 className="text-neutral-600 dark:text-neutral-200 text-2xl font-semibold">Lucas Daniel Fuentes</h2>
+        <p className="text-neutral-400 dark:text-neutral-400 text-md">Frontend Developer | JavaScript | ReactJS | HTML • CSS</p>
+      </div>
+    </section>
+  )
+}
+
+export { Hero }
