0621969d4b224f4bb74e0b52242a08f5d9f6d7e5
Lucas Fuentes <lucascarpii2002@gmail.com>
2023-12-08 18:53:46 -0300

    <style> Agregar borde inferior al Hero y separar elementos del Navbar

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -1,10 +1,10 @@
 
 const Hero = () => {
   return(
-    <section className="grid grid-cols-[100px_auto] gap-4 sm:py-6">
+    <section className="grid grid-cols-[100px_auto] gap-4 sm:py-6 border-b dark:border-neutral-800">
       <img src="/favicon.svg" alt="" className="bg-neutral-800 rounded-full" />
       <div className="flex flex-col items-start justify-center">
-        <h2 className="text-neutral-600 dark:text-neutral-200 text-2xl font-semibold">Lucas Daniel Fuentes</h2>
+        <h2 className="text-neutral-600 dark:text-[#D2C2B0] text-2xl font-semibold">Lucas Daniel Fuentes</h2>
         <p className="text-neutral-400 dark:text-neutral-400 text-md">Frontend Developer | JavaScript | ReactJS | HTML • CSS</p>
       </div>
     </section>
--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -2,12 +2,12 @@ import { DarkModeBtn } from "../DarkModeBtn";
 
 const Navbar = () => {
   return (
-    <nav className="flex justify-center w-full py-6">
+    <nav className="flex items-center justify-between w-full py-6">
       <ul className="flex gap-4 h-full text-neutral-600 dark:text-neutral-200">
         <li className="flex items-center ">Sobre mi</li>
-        <li className="flex items-center ">Experiencia</li>
-        <DarkModeBtn />
+        <li className="flex items-center ">Proyectos</li>
       </ul>
+      <DarkModeBtn />
     </nav>
   );
 }
