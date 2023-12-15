f4a65c85231a4b28f28e90d9a3a61e37898505bb
Lucas Fuentes <lucascarpii2002@gmail.com>
2023-12-15 18:33:32 -0300

    <fix> Cambiar mayusculas, corregir ruta de favicon

--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -4,7 +4,7 @@ const projects = [
   {
     title: 'Campus Virtual ISEI',
     desc: 'Esta plataforma fue desarrollada para el Instituto Superior ISEI con el objetivo de facilitar el aprendizaje y la enseñanza, por medio de cursos y contenidos online.',
-    techs: 'ReactJs • Vite • TailwindCss • JS',
+    techs: 'ReactJS • Vite • TailwindCSS • JS',
     slides: [
       "/public/campusvi/index.png",
       "/public/campusvi/cursos.png",
@@ -15,7 +15,7 @@ const projects = [
   {
     title: 'Distri DAF - Control de gastos',
     desc: 'El objetivo de esta aplicacion fue solucionar el problema que tenia la empresa para controlar el balance de gastos e ingresos.',
-    techs: 'SvelteKit • Vercel • Vite • TailwindCss • JS',
+    techs: 'SvelteKit • Vercel • Vite • TailwindCSS • JS',
     slides: [
       "/public/distri-daf/home.png",
       "/public/distri-daf/clients.png",
